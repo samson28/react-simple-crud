ffc414e96c298a64bf7fca72275de6800fd78fe6
samson28 <58706301+samson28@users.noreply.github.com>
2023-07-23 16:11:45 +0000

    feat: search

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,13 @@
 import { faCheckCircle, faCircle } from '@fortawesome/free-regular-svg-icons';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 import { checkArticleApi, deleteArticleApi, getArticlesApi } from '../app/articleService';
 
 
 function Article() {
+
+    const [query,setQuery] = useState("");
 
     const [state, setState] = useState({
         articles:[],
@@ -25,6 +27,7 @@ function Article() {
             let theTotalPages = Math.floor(totalElement/size);
             if(totalElement % size !=0) ++theTotalPages;
             setState({
+                ...state,
                 articles:resp.data,
                 keyword:keyword,
                 currentPage:page,
@@ -63,60 +66,84 @@ function Article() {
         handleGetArticles(state.keyword,page,state.pageSize);
     }
 
+    const handleSearch=(event)=>{
+        event.preventDefault();
+        handleGetArticles(query,1,state.pageSize);
+    }
+
   return (
     <div className='container-fluid'>
-        <table className="table">
-            <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Price</th>
-                <th scope="col">Available</th>
-                <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody className="table-group-divider">
-                {
-                    state.articles.map((article) => (
-                        <tr key={article.id}>
-                            <th scope="row">{article.id}</th>
-                            <td>{article.name}</td>
-                            <td>{article.price}</td>
-                            <td>
-                                <button 
-                                onClick={() => handleCheckArticle(article)}
-                                className='btn btn-outline-success'>
-                                    <FontAwesomeIcon icon={article.available?faCheckCircle:faCircle}></FontAwesomeIcon>
-                                </button>
-                            </td>
-                            <td>
-                                <button 
-                                onClick={() => handleDeleteArticle(article)}
-                                className='btn btn-outline-danger'>
-                                        <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
-                                </button>
-                            </td>
+        <div className="card" >
+            <div className="card-body">
+                <form onSubmit={handleSearch}>
+                    <div className='row g-2'>
+                        <div className="col-auto">
+                            <input 
+                            onChange={(e)=>setQuery(e.target.value)}
+                            value={query}
+                            className="form-control"/>
+                        </div>
+                        <div className="col-auto">
+                            <button className="btn btn-success">
+                                <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div className="card-body">
+                <table className="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Available</th>
+                        <th scope="col"></th>
                         </tr>
+                    </thead>
+                    <tbody className="table-group-divider">
+                        {
+                            state.articles.map((article) => (
+                                <tr key={article.id}>
+                                    <th scope="row">{article.id}</th>
+                                    <td>{article.name}</td>
+                                    <td>{article.price}</td>
+                                    <td>
+                                        <button 
+                                        onClick={() => handleCheckArticle(article)}
+                                        className='btn btn-outline-success'>
+                                            <FontAwesomeIcon icon={article.available?faCheckCircle:faCircle}></FontAwesomeIcon>
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button 
+                                        onClick={() => handleDeleteArticle(article)}
+                                        className='btn btn-outline-danger'>
+                                                <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+            <div className="card-footer">
+                <ul className='nav nav-pills'>
+                    { 
+                        new Array(state.totalPages).fill(0).map((v,index)=>(
+                            
+                            <li key={index + 1}>
+                                <button 
+                                onClick={()=>handleGoToPage(index + 1)}
+                                className={((index+1)==state.currentPage)?'btn btn-info ms-1':'btn btn-outline-info ms-1'}>{index + 1}</button>
+                            </li>
                         ))
                     }
-            </tbody>
-        </table>
-        
-        <ul className='nav nav-pills'>
-            {
-                
-                new Array(state.totalPages).fill(0).map((v,index)=>(
-                    
-                    <li key={index + 1}>
-                        <button 
-                        onClick={()=>handleGoToPage(index + 1)}
-                        className={((index+1)==state.currentPage)?'btn btn-info ms-1':'btn btn-outline-info ms-1'}>{index + 1}</button>
-                    </li>
-                ))
-            }
-        </ul>
-        
-
+                </ul>
+            </div>
+        </div>  
     </div>
 
   )
